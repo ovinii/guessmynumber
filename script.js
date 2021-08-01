e585b8fa9e128490f86d43cccbd82a6ce060c55f
Vinícius <viniciusnascimento__@outlook.com>
2021-08-01 08:12:14 -0300

    refactoring

--- script.js
+++ script.js
@@ -6,58 +6,50 @@ let secretNumber = Math.trunc(Math.random() * 20 + 1)
 let score = 20;
 let highscore = 0;
 
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+
+function resetScreen() {
+  setTimeout(() => {
+    document.body.style.backgroundColor = '#222'
+    document.querySelector('.number').textContent = '?'
+  }, 1000)
+}
+
 //check button
 const check = document.querySelector('.check')
 check.addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value)
-  console.log(guess, secretNumber)
-  console.log(typeof(guess), typeof(secretNumber))
 
-  if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '✅ Correct Number!'
+  if (!guess) {
+    displayMessage('⛔ Invalid Number')
+    document.body.style.backgroundColor = 'red'
+    document.querySelector('.number').textContent = 'X'
+    wrongEffect.play()
+    document.querySelector('.score').textContent = score
+    score--
+    resetScreen()
+  } else if (guess === secretNumber) {
+    displayMessage('✅ Correct Number!')
     document.querySelector('.number').textContent = secretNumber
     document.body.style.backgroundColor = '#60b347'
     document.querySelector('.number').style.width = '30rem';
     correctEffect.play()
 
-    if(score > highscore) {
+    if (score > highscore) {
       highscore = score
       document.querySelector('.score').textContent = highscore
       document.querySelector('.highscore').textContent = highscore
     }
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too low'
-    score--
+  } else if (guess != secretNumber) {
+    displayMessage(guess < secretNumber ? '📉 Too low' : '📈 Too high')
     document.body.style.backgroundColor = 'red'
     document.querySelector('.number').textContent = 'X'
-    wrongEffect.play()
-    setTimeout(() => {
-      document.body.style.backgroundColor = '#222'
-      document.querySelector('.number').textContent = '?'
-    }, 1000)
     document.querySelector('.score').textContent = score
-  } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too high'
-    document.body.style.backgroundColor = 'red'
-    document.querySelector('.number').textContent = 'X'
-    wrongEffect.play()
-    setTimeout(() => {
-      document.body.style.backgroundColor = '#222'
-      document.querySelector('.number').textContent = '?'
-    }, 1000)
     score--
-    document.querySelector('.score').textContent = score
-  } else {
-    document.querySelector('.message').textContent = '⛔ Invalid Number'
-    document.body.style.backgroundColor = 'red'
-    document.querySelector('.number').textContent = 'X'
+    resetScreen()
     wrongEffect.play()
-    setTimeout(() => {
-      document.body.style.backgroundColor = '#222'
-      document.querySelector('.number').textContent = '?'
-    }, 1000)
-    score--
-    document.querySelector('.score').textContent = score
   }
 })
 
@@ -65,7 +57,7 @@ check.addEventListener('click', function () {
 const again = document.querySelector('.again')
 again.addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20 + 1)
-  document.querySelector('.message').textContent = 'Start guessing...'
+  displayMessage('Start guessing...')
   document.querySelector('.number').textContent = '?'
   document.querySelector('.guess').value = ''
   document.querySelector('.number').style.width = '15rem';
